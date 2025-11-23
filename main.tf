bd4847c739a03af4cce77865c9c8ab40f334ca38
Sat2022 <sathish.kf87@outlook.com>
2025-11-23 18:44:56 +1100

    Deploy Version 2 with NewFeature

--- main.tf
+++ main.tf
@@ -44,9 +44,12 @@ resource "azurerm_windows_web_app" "test_app" {
   app_settings = {
     "Environment" = "Test"
     "CreatedBy"   = "Terraform"
+    "Version"     = "2.0"      # Changed to 2.0
+    "NewFeature"  = "Enabled"  # NEW setting
   }
 
   tags = {
     Lab = "Azure app services"
+    "Env" = "v2-Beta"          # Changed tag
   }
 }
